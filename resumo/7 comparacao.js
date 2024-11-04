42963dbb78252ee4df0d2121bc96d7b0f8d93be9
DanielBorbafs <dev.danielborba@gmail.com>
2024-11-04 20:23:15 -0300

    chore: intro

---
+++ resumo/7 comparacao.js
@@ -0,0 +1,18 @@
+/* 
+==
+===
+> >=
+< <=
+!= !===
+*/
+
+let a = 200;
+let b = toString('200');
+
+if (a == b) {
+  console.log('verdadeiro');
+} else {
+  console.log('false');
+}
+
+// object is()
