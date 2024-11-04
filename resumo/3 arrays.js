28c1d324c3f092ee2b5ff70ac103b9f7481e2b1b
DanielBorbafs <dev.danielborba@gmail.com>
2024-11-04 20:14:54 -0300

    intro

---
+++ resumo/3 arrays.js
@@ -0,0 +1,15 @@
+// Arrays
+
+let colecao = ['Civic', 'Astra', 'Fusca', 'Hilux'];
+console.log(colecao);
+colecao.push('Prisma', 'Subaru');
+console.log(colecao);
+
+colecao.pop();
+console.log(colecao);
+
+colecao.shift();
+console.log(colecao);
+
+colecao.unshift('Onix', 'New fiesta', 'Gol bolinha', 'Corsinha');
+console.log(colecao);
---
+++ resumo/5 maps.js
@@ -0,0 +1,4 @@
+const pessoa = new Map();
+pessoa.set(123, 'valor inteiro');
+pessoa.set({}, 'Chaves vazias');
+console.log(pessoa);
