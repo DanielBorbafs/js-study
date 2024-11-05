0232e896f0a247466b488cc4808340038c82e21d
DanielBorbafs <dev.danielborba@gmail.com>
2024-11-04 22:34:26 -0300

    loops

--- resumo/3 arrays.js
+++ resumo/3 arrays.js
@@ -13,3 +13,25 @@ console.log(colecao);
 
 colecao.unshift('Onix', 'New fiesta', 'Gol bolinha', 'Corsinha');
 console.log(colecao);
+
+// Interação em array
+const frutas = ['maçã', 'banana', 'laranja'];
+
+frutas.forEach((fruta) => {
+  console.log(fruta);
+});
+
+// map(): Cria um novo array com os resultados da chamada de uma função em cada elemento do array original.
+const numeros = [1, 2, 3, 4];
+const quadrados = numeros.map((numero) => numero * numero);
+console.log(quadrados); // [1, 4, 9, 16]
+
+// filter(): Cria um novo array com todos os elementos que passam em um teste (condição) fornecido por uma função.
+const numeross = [1, 2, 3, 4, 5];
+const pares = numeross.filter((numero) => numero % 2 === 0);
+console.log(pares); // [2, 4]
+
+//reduce(): Executa uma função redutora em cada elemento do array, resultando em um único valor.
+const numerosss = [1, 2, 3, 4];
+const soma = numerosss.reduce((acumulador, numero) => acumulador + numero, 0);
+console.log(soma); // 10
--- resumo/8 loops.js
+++ resumo/8 loops.js
@@ -3,3 +3,62 @@
 for (i = 0; i < 2; i++) {
   console.log(`sou o número ${i}`);
 }
+
+// for
+const vendasDiarias = [700, 600, 700, 600, 110, 250, 780];
+let totalSemanal = 0;
+
+for (let i = 0; i < vendasDiarias.length; i++) {
+  totalSemanal += vendasDiarias[i];
+  /*
+  Durante cada iteração do loop, estamos adicionando o valor atual de vendasDiarias[i] à variável totalSemanal. 
+  O operador += é uma forma abreviada de escrever totalSemanal = totalSemanal + vendasDiarias[i]. Assim, em cada iteração,
+  o valor da venda diária correspondente é somado ao total.
+*/
+}
+console.log(`O total de vendas da semana foi de ${totalSemanal}`);
+
+// While
+/*
+O while loop é uma estrutura de controle em JavaScript que permite executar um bloco de código repetidamente
+enquanto uma condição especificada for verdadeira. Ele é especialmente útil quando o número de iterações não é conhecido previamente
+e depende de uma condição que pode mudar durante a execução do loop
+*/
+
+let estoque = 5;
+const limite = 10;
+while (estoque < limite) {
+  console.log(`estoque atual ${estoque}. Precisamos reabastecer!`);
+  estoque++;
+}
+console.log(`estoque reabastecido para: ${estoque}`);
+
+//do while
+/*
+O código é sempre executado pelo menos uma vez, pois a condição é verificada após a execução do código.*/
+/*let numero;
+do {
+  numero = parseInt(prompt('Digite um número positivo:'));
+} while (numero <= 0);
+console.log(`Você digitou o número positivo: ${numero}`);
+*/
+
+// for...of
+// O loop for...of é usado para iterar sobre elementos de objetos iteráveis, como arrays, strings, e outras coleções.
+
+const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (const numero of numeros) {
+  console.log(numero);
+}
+
+// for... in
+// O loop for...in é usado para iterar sobre as chaves (ou propriedades) de um objeto.
+const pessoa = {
+  nome: 'Alice',
+  idade: 30,
+  cidade: 'São Paulo',
+};
+
+for (const chave in pessoa) {
+  console.log(`${chave}: ${pessoa[chave]}`);
+}
