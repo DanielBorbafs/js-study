adc847367a54924aea369b3172427ed4d8f148c6
Danielborbafs <dev.danielborba@gmail.com>
2024-11-22 20:12:32 -0300

    Desvio padrao: function

--- resumo/Exercicios/desvioPadrao.js
+++ resumo/Exercicios/desvioPadrao.js
@@ -13,3 +13,18 @@ const somaQuadrados = listaNumeros.reduce((acc, currentValue) => acc + Math.pow(
 const desvioPadrao = Math.sqrt(somaQuadrados / numeroOcorrencia);
 
 console.log("Desvio Padrão: ", desvioPadrao.toFixed(2));
+
+// function
+function calculaDesvio(a, b, c) {
+    let arr = [a, b, c]
+    let numeroOcorrencia = arr.length
+    const somaArray = arr.reduce((acc, currentValue) => acc + currentValue, 0);
+    const media = somaArray / numeroOcorrencia
+
+    const somaQuadrados = arr.reduce((acc, currentValue) => acc + Math.pow(currentValue - media, 2), 0);
+
+    const desvioPadrao = Math.sqrt(somaQuadrados / numeroOcorrencia)
+    console.log("Desvio Padrão: ", desvioPadrao.toFixed(2))
+}
+
+calculaDesvio(5, 10, 15)
