4972242218d1c1b490ec22580fe5269067cef5e3
DanielBorbafs <dev.danielborba@gmail.com>
2025-02-10 15:26:20 -0300

    chore: organizando o repositório

--- intro/4 sets.js
+++
@@ -1,6 +0,0 @@
-// Sets
-const listaDeCompras = new Set(['Arroz', 'Feijao', 'Macarrão']);
-listaDeCompras.add('Farofa');
-listaDeCompras.add('Farofa2');
-
-console.log(listaDeCompras);
--- intro/5 maps.js
+++
@@ -1,4 +0,0 @@
-const pessoa = new Map();
-pessoa.set(123, 'valor inteiro');
-pessoa.set({}, 'Chaves vazias');
-console.log(pessoa);
