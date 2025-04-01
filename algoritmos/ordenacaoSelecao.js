1c041a0e32e6126419fc9a4745d67aaace0dfb04
DanielBorbafs <dev.danielborba@gmail.com>
2025-04-01 19:02:49 -0300

    recursividade

--- algoritmos/ordenacaoSelecao.js
+++ algoritmos/ordenacaoSelecao.js
@@ -1,9 +1,22 @@
-function fatorial(x) {
-  if (x === 1) {
-    return 1;
-  } else {
-    return x * fatorial(x - 1);
+/*
+A cada passo, seleciona o menor elemento do array não ordenado e o coloca na posição correta.
+*/
+
+// O(n²) - Ele percorre cada item do array mesmo se estiver ordenado, pois ele nao identifica se já está ordenado
+function selectionSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    let minIdx = i; // Assume que o primeiro não ordenado é o menor
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j; // Encontrou um elemento menor
+      }
+    }
+    // Troca os elementos (destructuring assignment)
+    [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
   }
+  return arr;
 }
 
-console.log(fatorial(4));
+// Exemplo de uso:
+console.log(selectionSort([5, 3, 1, 4, 2])); // Saída: [1, 2, 3, 4, 5]
